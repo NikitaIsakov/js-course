57dc39789fc80b61d6839e276c3c599d3089115a
UserGodz <isakov4352@gmail.com>
2019-04-06 23:57:07 +0700

    Добавлен 3 урок. Добавлен второй этап домашней работы

--- homeworks/project part2/main.js
+++ homeworks/project part2/main.js
@@ -36,21 +36,36 @@ function chooseExpenses() {
 
 chooseExpenses();
 
-appData.moneyPerDay = (appData.budget / 30).toFixed();
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert('Ежедневный бюджет:' + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-    console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('Высокий уровень достатка');
-} else {
-    console.log('Произошла ошибка');
+function detectDayBudget () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert('Ежедневный бюджет:' + appData.moneyPerDay);
 }
+
+
+detectDayBudget();
+
+function detectLevel () {
+    if (appData.moneyPerDay < 100) {
+        console.log('Минимальный уровень достатка');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log('Средний уровень достатка');
+    } else if (appData.moneyPerDay > 2000) {
+        console.log('Высокий уровень достатка');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+detectLevel();
+
+function chooseOptExpenses() {
+    for (let i = 0; i < 3; i++) {
+        let chooseExpenses = prompt('Введите необязательную статью расходов', '');
+        appData.optionalExpenses[i] = chooseExpenses;
+    }
+}
+
+chooseOptExpenses();
 
 function checkSavings() {
     if(appData.savings == true) {
