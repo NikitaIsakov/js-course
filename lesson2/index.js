3acba4794939c779cd6f6e3b80e2e0cbda60ffe6
UserGodz <isakov4352@gmail.com>
2019-04-06 11:32:25 +0700

    Добавлены циклы для второго урока

--- lesson2/index.js
+++ lesson2/index.js
@@ -27,3 +27,22 @@ switch (num) {
     default:
         console.log('Что-то пошло не так');
 }
+
+// let num = 50;
+while (num < 55) {
+    console.log(num);
+    num++;
+};
+
+do {
+    console.log(num);
+    num++; 
+} 
+while (num < 55);
+
+for (let i = 0; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
