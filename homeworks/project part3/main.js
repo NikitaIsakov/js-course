5f3a51c8726e6c4405ddc944fae654f48b0c6957
UserGodz <isakov4352@gmail.com>
2019-04-07 10:37:40 +0700

    Добавлен 5 урок. Обновлен этап 3 в калькуляторе

---
+++ homeworks/project part3/main.js
@@ -0,0 +1,105 @@
+let money, time;
+
+function start() {
+    money = +prompt('Ваш бюджет на месяц?', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD');
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
+    }
+}
+
+// start();
+
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: true,
+    chooseExpenses: function() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Введите обязательную статью расходов"),
+                b = prompt('Во сколько обойдется?');
+            if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+                appData.expenses[a] = b;
+            } else {
+                i = i - 1;
+            }
+        }
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert('Ежедневный бюджет:' + appData.moneyPerDay);
+    },
+    detectLevel: function() {
+        if (appData.moneyPerDay < 100) {
+            console.log('Минимальный уровень достатка');
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log('Средний уровень достатка');
+        } else if (appData.moneyPerDay > 2000) {
+            console.log('Высокий уровень достатка');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    chooseExpenses: function () {
+        for (let i = 0; i < 3; i++) {
+            let chooseExpenses = prompt('Введите необязательную статью расходов', '');
+            appData.optionalExpenses.i = chooseExpenses;
+        }
+    },
+    checkSavings() {
+        if(appData.savings == true) {
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент?");
+    
+            appData.monthIncome = save/100/12*percent;
+            alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)', '');
+        // console.log(items);
+        appData.icome = items.split(', ');
+
+        items.forEach(function(item, i, items){
+            if (isNaN(i) && items == "" && items == null) {
+                console.log('vrode')
+                // i = i - 1;
+                item = item - 1;
+                items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)', '');
+            } else {
+                // i = i - 1;
+                console.log('ffffff');
+            }
+        })
+
+        // for (let i = 0; i < 1; i++) {
+            
+            
+            
+            
+        // }
+
+        // while (isNaN(items) && items !== "" && items == null) {
+        //     console.log(324);
+        // }
+
+        // for (let i = 0; i < 1; i++) {
+        //     if ( (typeof(items)) === 'string' && (typeof(items)) != null && (typeof(b)) != null && items != '') {
+        //         appData.income.push(prompt('Может что-то ещё?'));
+        //         appData.icome.sort();
+        //     } else {
+        //         i = i - 1;
+        //     }
+        // }
+        // appData.icome.sort();
+                
+    }
+}
+
+
+appData.chooseIncome();
---
+++ lesson5/js/script.js
@@ -0,0 +1,6 @@
+let menu =  document.querySelector('.menu');
+
+
+document.body.style.background = 'url(img/apple_true.jpg) center no-repeat';
+document.querySelector('.title').innerHTML = 'Мы продаем только подлинную технику Apple';
+document.querySelector('.adv').remove();
